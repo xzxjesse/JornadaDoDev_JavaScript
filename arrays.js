eac656defeabe32e694f159830d88fcdfd1a5e70
Jéssica Eveline <110188236+xzxjesse@users.noreply.github.com>
2024-01-11 11:49:27 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let cargos = ["administradora", "médica", "advogada", "analista de sistemas"];
+
+console.log(cargos);
+
+//cargos[1] = "contadora";
+
+//alert(cargos[3]);
+
+//método de adicionar item em arrays
+cargos.push("devops")
+
+//método de remover item em arrays
+//vazio = ultimo item
+cargos.pop()
