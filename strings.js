06ad1e90698bd977fae859cb2c2b31adba19d545
Jéssica Eveline <110188236+xzxjesse@users.noreply.github.com>
2024-01-11 11:40:06 -0300

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,26 @@
+// como declarar uma string
+let string = "texto";
+
+let mensagem1 = "Olá!";
+let mensagem2 = "Tudo bem?";
+
+// contatenação de strings
+let concatenar = mensagem1 + " " + mensagem2;
+
+let pessoa = "Jesse";
+let mensagem3 = pessoa + ": " + "Olá, tudo bem?"
+let mensagem4 = "Olá, " + pessoa + "! " + mensagem2;
+
+let mensagem5 = `${pessoa} Olá, seja bem vinda!`;
+
+//metodo para visualização da variavel
+console.log(concatenar);
+console.log(mensagem3);
+console.log(mensagem4);
+console.log(mensagem5);
+
+//exercício
+
+let usuario = prompt("Qual é o seu nome?");
+alert(`${usuario}, bem vinda!je`);
+console.log(usuario);
