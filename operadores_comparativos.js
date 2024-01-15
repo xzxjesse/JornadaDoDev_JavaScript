e13a35771ef981fc27dfac240b0272cf643b0045
xzxjesse <jessicaeveline121@gmail.com>
2024-01-15 10:31:06 -0300

    operadores comparativos

---
+++ operadores_comparativos.js
@@ -0,0 +1,17 @@
+// comparar se é identico
+a===b
+
+// comparar é diferente
+a!==b
+
+// maior que
+10<20
+
+// menor que
+10>20
+
+// maior ou igual
+10>=10
+
+// menor ou igual
+10<=20
---
+++ sistema_notas.js
@@ -0,0 +1,8 @@
+let nota1 = Number(prompt("Digite a sua primeira nota"));
+let nota2 = Number(prompt("Digite a sua segunda nota"));
+
+let resultado = (nota1 + nota2) /2;
+
+console.log(resultado)
+
+alert (resultado>=5);
