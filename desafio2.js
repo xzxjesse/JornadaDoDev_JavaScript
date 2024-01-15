0b7015cf8f225826b1e964d78507fda380edba9e
xzxjesse <jessicaeveline121@gmail.com>
2024-01-15 10:18:50 -0300

    Operadores Aritméticos

---
+++ desafio2.js
@@ -0,0 +1,11 @@
+// pedro tem 2 caixas de pirulitos, com 20 unidades cada, e precisa dividir com 5 amigos.
+// com quantos pirulitos cada amigo ficou?
+
+let caixa = 2;
+let conteudo = 20;
+let pirulitos = caixa * conteudo;
+let amigos = 5;
+
+let individual = pirulitos / amigos;
+
+console.log(individual);
---
+++ operadores_aritmeticos.js
@@ -0,0 +1,15 @@
+// soma
+10+10
+
+// subtração
+10-5
+
+// multiplicar
+10*5
+
+// dividir
+5/10
+
+let calculo = 10;
+let calculo2 = 5;
+let resultado_multiplicado = calculo * calculo2;
